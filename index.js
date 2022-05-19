73a4e7f2a6dca18144d42876072246fef4de1adb
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2022-05-19 22:14:33 +0530

    refactor: every thing in classes

--- index.js
+++ index.js
@@ -1,36 +1,70 @@
-/** @type {HTMLFormElement} */
-const form = document.querySelector('#todo-add-form');
-/** @type {HTMLInputElement} */
-const todo_content = document.querySelector('#todo-content-textbox');
-/** @type {HTMLUListElement} */
-const todo_list = document.querySelector('#todo-list');
-/** @type {HTMLParagraphElement} */
-const no_todo_msg = document.querySelector('#no-todo-msg');
+class TodoListNode {
+	/** @type {HTMLUListElement} */
+	todo_list;
+	/** @type {HTMLParagraphElement} */
+	no_todo_msg;
 
-const update_no_todo_msg = () => {
-	if (todo_list.hasChildNodes()) no_todo_msg.style.setProperty('display', 'none');
-	else no_todo_msg.style.setProperty('display', 'block');
+	/**
+	 * @param {string} selector
+	 */
+	constructor(todo_list_selector, no_todo_msg_selector) {
+		this.todo_list = document.querySelector(todo_list_selector);
+		this.no_todo_msg = document.querySelector(no_todo_msg_selector);
+	}
+
+	/**
+	 * @param {Todo} todo
+	 */
+	append_todo(todo) {
+		this.todo_list.appendChild(todo.todo_node);
+
+		this.#update_no_todo_msg();
+	}
+
+	/**
+	 * @param {Todo} todo
+	 */
+	remove_todo(todo) {
+		this.todo_list.removeChild(todo.todo_node);
+
+		this.#update_no_todo_msg();
+	}
+
+	#update_no_todo_msg() {
+		if (this.todo_list.hasChildNodes()) this.no_todo_msg.style.setProperty('display', 'none');
+		else this.no_todo_msg.style.setProperty('display', 'block');
+	}
+
+	remove_all_todos() {
+		while(this.todo_list.lastChild) this.todo_list.removeChild(this.todo_list.lastChild);
+	}
 }
 
+const todo_list_node = new TodoListNode('#todo-list', '#no-todo-msg');
+
 class Todo {
+	/** @type {number} */
+	id;
 	/** @type {string} */
 	title;
+
+	/** @type {(id: number) => void} */
+	on_delete;
+
 	/** @type {HTMLLIElement} */
 	todo_node;
 
-	/** @param title {string} */
-	constructor(title) {
+	/**
+	 * @param id {number}
+	 * @param title {string}
+	 * @param on_delete {(id: number) => void}
+	 * */
+	constructor(id, title, on_delete) {
+		this.id = id;
 		this.title = title;
+		this.on_delete = on_delete;
+
 		this.todo_node = this.#create_todo_node(this.title);
-	}
-
-	static handle_todo_delete = (e) => {
-		e.preventDefault();
-		const btn = e.currentTarget;
-
-		btn.parentElement.remove();
-
-		update_no_todo_msg();
 	}
 
 	/** @param title {string} */
@@ -46,9 +80,13 @@ class Todo {
 
 	#create_del_btn_node() {
 		const todo_del_btn = document.createElement('button');
+
 		todo_del_btn.classList.add('todo-del-btn', 'icon-btn');
 		todo_del_btn.textContent = '✓';
-		todo_del_btn.addEventListener('click', Todo.handle_todo_delete);
+
+		todo_del_btn.addEventListener('click', () => {
+			this.on_delete(this.id);
+		})
 
 		return todo_del_btn;
 	}
@@ -65,20 +103,48 @@ class Todo {
 }
 
 class TodoList {
+	/** @type {number} */
+	todo_count = 0;
+	/** @type {Todo[]} */
+	todos = [];
+
 	/** @param todo {Todo} */
 	#render_todo(todo) {
-		todo_list.append(todo.todo_node);
-
-		update_no_todo_msg();
+		todo_list_node.append_todo(todo);
 	}
 
 	/** @param title {string} */
 	add_todo(title) {
-		this.#render_todo(new Todo(title));
+		this.todo_count++;
+
+		const todo = new Todo(this.todo_count, title, (id) => this.#delete_todo(id));
+		this.todos.push(todo);
+
+		this.#render_todo(todo);
+	}
+
+	#delete_todo(id) {
+		const todo_to_delete = this.todos.splice(
+			this.todos.findIndex((todo) => todo.id === id),
+			1
+		)[0]
+
+		todo_list_node.remove_todo(todo_to_delete);
+	}
+
+	#rerender_all_todos() {
+		todo_list_node.remove_all_todos();
+
+		this.todos.forEach(this.#render_todo);
 	}
 }
 
 const TODO_LIST = new TodoList();
+
+/** @type {HTMLFormElement} */
+const form = document.querySelector('#todo-add-form');
+/** @type {HTMLInputElement} */
+const todo_content = document.querySelector('#todo-content-textbox');
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -91,6 +157,4 @@ form.addEventListener('submit', (e) => {
 
 window.addEventListener('load', () => {
 	TODO_LIST.add_todo('Sample TODO');
-
-	update_no_todo_msg();
 })
